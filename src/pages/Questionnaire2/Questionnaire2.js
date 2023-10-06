5afe264814dd3a221a166b90a19826a73f2d3884
Emily Oudyk <eoudyk@gmail.com>
2023-10-06 13:59:55 -0400

    add footer for questionnaire pages

--- src/pages/Questionnaire2/Questionnaire2.js
+++ src/pages/Questionnaire2/Questionnaire2.js
@@ -4,6 +4,7 @@ import OptionCard from "../../components/OptionCard/OptionCard";
 import ProgressBar from "../../components/ProgressBar/ProgressBar";
 import backArrowIcon from "../../assets/icons/back-arrow.svg";
 import "./questionnaire2.scss";
+import PageFooter from "../../assets/logo/footer-image.svg";
 
 const Questionnaire = () => {
   const options = ["Conservative", "Balanced", "Aggressive"];
@@ -20,30 +21,33 @@ const Questionnaire = () => {
   };
 
   return (
-    <section className="questionnaire2">
-      {/* question section */}
-      <h2> How confortable are you with investment rist? </h2>
-      {options.map((option) => {
-        return <OptionCard answer={option} onClick={handleClick} />;
-      })}
+    <>
+      <section className="questionnaire2">
+        {/* question section */}
+        <h2> How confortable are you with investment rist? </h2>
+        {options.map((option) => {
+          return <OptionCard answer={option} onClick={handleClick} />;
+        })}
 
-      <div className="questionnaire2__progress-bar">
-        {testData.map((item, i) => (
-          <ProgressBar
-            key={i}
-            bgcolor={item.bgcolor}
-            completed={item.completed}
-          />
-        ))}
-      </div>
+        <div className="questionnaire2__progress-bar">
+          {testData.map((item, i) => (
+            <ProgressBar
+              key={i}
+              bgcolor={item.bgcolor}
+              completed={item.completed}
+            />
+          ))}
+        </div>
 
-      {/* next question button section */}
-      <div className="questionnaire2__bottom-container">
-        <button className="questionnaire2__button" onClick={handleBackButton}>
-          <img src={backArrowIcon} />
-        </button>
-      </div>
-    </section>
+        {/* next question button section */}
+        <div className="questionnaire2__bottom-container">
+          <button className="questionnaire2__button" onClick={handleBackButton}>
+            <img src={backArrowIcon} />
+          </button>
+        </div>
+      </section>
+      <img src={PageFooter} />
+    </>
   );
 };
 
