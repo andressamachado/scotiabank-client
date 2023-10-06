4e1c4a825cc02a530e096100f7711ca0d9a029e9
Emily Oudyk <eoudyk@gmail.com>
2023-10-06 09:39:28 -0400

    add page routes, page folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import Header from "./components/Header/Header";
 import "./app.scss";
 import Questionnaire from "./components/Questionnaire/Questionnaire";
+import ChatPage from "./pages/ChatPage/ChatPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Questionnaire />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Questionnaire />} />
+          <Route path="/advisors" element={<Advisors />} />
+          <Route path="/chatnow" element={<ChatPage />} />
+        </Routes>
+        <Questionnaire />
+      </BrowserRouter>
     </div>
   );
 }
