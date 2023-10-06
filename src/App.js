bab45e77f2e8bf5f4f9ac5d7303c8dd9bfd13b58
andressamachado <andressa.pamachado@gmail.com>
2023-10-06 01:02:52 -0400

    Add header including nav bar and banner

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Header from "./components/Header/Header";
 import "./app.scss";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
