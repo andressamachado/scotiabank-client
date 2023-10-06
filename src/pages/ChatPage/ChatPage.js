c713433152317ddec018ccc777b123c12209c30e
Emily Oudyk <eoudyk@gmail.com>
2023-10-06 12:31:35 -0400

    add chat page styling

--- src/pages/ChatPage/ChatPage.js
+++ src/pages/ChatPage/ChatPage.js
@@ -1,19 +1,8 @@
-// import ChatHeader from "../../components/ChatHeader/ChatHeader";
-
-// export default ChatFeature = () => {
-//   return (
-//     <>
-//       <ChatHeader />
-//       <div className="Chatbox area" />
-//       <ChatFooter />
-//     </>
-//   );
-// };
-
-// src/Chat/Chat.js
-
 import React, { useState } from "react";
-import "./Chat.css";
+import "./ChatPage.scss";
+import backarrow from "../../assets/icons/prime_arrow-up.svg";
+import ProfileImage from "../../assets/icons/Ellipse-181.svg";
+import paperClip from "../../assets/icons/paperclip-attach.svg";
 
 const Chat = () => {
   const [messages, setMessages] = useState([]);
@@ -27,24 +16,50 @@ const Chat = () => {
   };
 
   return (
-    <div className="chat-container">
-      <div className="messages">
-        {messages.map((message, index) => (
-          <div key={index} className={`message ${message.sender}`}>
-            {message.text}
+    <>
+      <header>
+        <img className="backarrow" src={backarrow} alt="back button" />
+        <div className="advisor">
+          <img
+            className="advisor__image"
+            src={ProfileImage}
+            alt="profile picture"
+          />
+          <div className="advisor__details">
+            <div className="advisor__name">Keanu Reeves</div>
+            <div className="advisor__response">
+              Typically replies within 24 hours
+            </div>
           </div>
-        ))}
+        </div>
+        <div className="feedback">Share Feedback</div>
+      </header>
+      <section className="chatSpace"></section>
+      <div className="chat-container">
+        <div className="messages">
+          {messages.map((message, index) => (
+            <div key={index} className={`message ${message.sender}`}>
+              {message.text}
+            </div>
+          ))}
+        </div>
+        <form className="message-form">
+          <input
+            type="text"
+            className="message-form__input"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Type a message..."
+          />
+          <img
+            className="message-form__attachement"
+            src={paperClip}
+            alt="attachements"
+          />
+          <button onClick={handleSendMessage}>Send</button>
+        </form>
       </div>
-      <div className="footer">
-        <input
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          placeholder="Type a message..."
-        />
-        <button onClick={handleSendMessage}>Send</button>
-      </div>
-    </div>
+    </>
   );
 };
 
