e599f645f2646773a86defeb58f5d656feeff87b
andressamachado <andressa.pamachado@gmail.com>
2023-10-06 15:21:19 -0400

    anything

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Questionnaire2 from "./pages/Questionnaire2/Questionnaire2";
 import Questionnaire3 from "./pages/Questionnaire3/Questionnaire3";
 import ChatPage from "./pages/ChatPage/ChatPage";
 import AvailableAdvisors from "./components/AvailableAdvisors/AvailableAdvisors";
+import AdvisorInfo from "./components/AdvisorInfo/AdvisorInfo";
 import "./app.scss";
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
           <Route path={`/questionnaire/2`} element={<Questionnaire2 />} />
           <Route path={`/questionnaire/3`} element={<Questionnaire3 />} />
           <Route path={`/advisors`} element={<AvailableAdvisors />} />
+          <Route path={`/advisor`} element={<AdvisorInfo />} />
           <Route path="/chat" element={<ChatPage />} />
         </Routes>
       </BrowserRouter>
